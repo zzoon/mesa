7b53051fe5fa1193b9ca71db38d1a39ad7e1b0af
Emil Velikov <emil.velikov@collabora.com>
2016-03-01 14:01:54 +0000

    glx/glvnd: remove unneeded extern C
    Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

--- src/glx/glxglvnd.h
+++ src/glx/glxglvnd.h
@@ -1,10 +1,6 @@
 #ifndef _glx_lib_glvnd_h_
 #define _glx_lib_glvnd_h_
 
-#if defined(__cplusplus)
-extern "C" {
-#endif
-
 #include "glvnd/libglxabi.h"
 
 extern __GLXapiExports __glXGLVNDAPIExports;
@@ -16,9 +12,5 @@ extern int __glXDispatchTableIndices[];
 extern const char *__glXDispatchTableStrings[];
 
 extern void __glXGLVNDInitDispatchFunctions(void);
-
-#if defined(__cplusplus)
-}
-#endif
 
 #endif
